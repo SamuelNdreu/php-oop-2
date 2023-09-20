041a1fc1340bf862e68dd1cedb038172d9ccfb93
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-20 21:34:28 +0200

    created repository and finished scheme of exercise

---
+++ models/ShoppingCart.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// class ShoppingCart {
+
+//     //fields
+//     protected $quantity;
+//     protected $totalPrice;
+
+//     //method Construct
+//     function __construct($_quantity, $_to)
+//     {
+        
+        
+//     }
+
+//     function 
+
+
+?>
